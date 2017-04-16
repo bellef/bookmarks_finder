0fa176f218c9c4565790323e30ab84fef1b9192c
Francois <francois@francoisbelle.com>
2017-04-16 18:09:36 +0200

    Highlight string that matches query

--- myscript.js
+++ myscript.js
@@ -1,14 +1,36 @@
-function buildBookmarkElement(title, url) {
+// Insert str2 into str1 at position
+function insertStr(str1, str2, position) {
+  return str1.substr(0, position) + str2 + str1.substr(position);
+}
+
+// Returns text with spans surrounding
+// Text that matches query string
+function highlightTextWithQuery(string, strQuery) {
+  var regexp = RegExp(strQuery.replace(/\s+/g, '.*'), 'i'); // Case insensitive
+  var match = string.match(regexp);
+  var spanOpen = "<span class='bm-highlight'>";
+  var spanClose = "</span>";
+  var html = "";
+
+  if (match) {
+    html = insertStr(string, spanOpen, match.index);
+    html = insertStr(html, spanClose, match.index + match[0].length + spanOpen.length);
+    return html;
+  }
+  return string;
+}
+
+// Builds HTML to render for one bookmark
+function buildBookmarkElement(title, url, query) {
   var html = '';
   html += "<div class='bm-elem'>" +
           "<a href='" + url + "'>" +
-            "<span class='bm-title'>" + title + "</span><br>" +
-            "<span class='bm-url'>" + url.substring(0, 99) +
+            "<span class='bm-title'>" + highlightTextWithQuery(title, query) + "</span><br>" +
+            "<span class='bm-url'>" + highlightTextWithQuery(url, query).substring(0, 99) +
           "</span></a>" +
           "</div>";
   return html;
 }
-
 
 // Ask background script to query on bookmarks
 function queryBookmarks() {
@@ -23,7 +45,7 @@ function queryBookmarks() {
       var title = bookmark['title'].length ? bookmark['title'] : bookmark['url'];
       var url = bookmark['url'];
 
-      htmlToInsert += buildBookmarkElement(title, url);
+      htmlToInsert += buildBookmarkElement(title, url, query);
     });
     htmlToInsert += "</div>";
 
